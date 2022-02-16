828cafcd95d1f31a7723cf67cc4f121c90e8715d
Harsha Nori <harsha.nori@live.com>
2022-02-16 13:52:00 -0800

    Upgrade CI pipeline to test all major OS. Switch to requirements.txt system to enable dependency caching in CI pipeline.

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    install_requires = [line.strip() for line in f]
+
 setup(
     name='adatest',
     version='0.0.2',
@@ -8,17 +11,5 @@ setup(
     author_email='scott.lundberg@microsoft.com',
     description='Adaptively test and debug any natural language machine learning model.',
     packages=find_packages(exclude=['user_studies', 'notebooks', 'client']),
-    install_requires=[
-        'nest_asyncio',
-        'aiohttp',
-        'aiohttp_session',
-        'aiohttp_security',
-        'numpy',
-        'pandas',
-        'sklearn',
-        'openai',
-        'sentence_transformers',
-        'checklist',
-        'shap',
-    ],
+    install_requires=install_requires
 )
