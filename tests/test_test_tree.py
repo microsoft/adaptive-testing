66b5ee0dc4e17480ad3f60c97b682b249d1a84ed
Scott Lundberg <scott.lundberg@microsoft.com>
2022-03-16 11:20:31 -0700

    Fix test tree test

--- tests/test_test_tree.py
+++ tests/test_test_tree.py
@@ -3,8 +3,10 @@ import adatest
 def test_simple_init():
     tree = adatest.TestTree()
 
-def test_simple_init_with_auto_save():
-    tree = adatest.TestTree("temp_test_tree.csv", auto_save=True)
+def test_simple_init_with_file():
+    tree = adatest.TestTree("temp_test_tree.csv")
 
 def test_simple_init_with_list():
-    tree = adatest.TestTree([("This is good", "should be", "POSITIVE"), ("This is bad", "should be", "NEGATIVE")])
+    tree = adatest.TestTree([
+        {"topic": "", "type": "{} should output {}", "value1": "This is good", "value2": "NEGATIVE"}
+    ])
