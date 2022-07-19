70992c6d42fdeb0d1f2ee5fb0c948217c27ca81e
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-07-19 11:25:08 -0400

    blacken...

--- tests/test_test_tree_browser.py
+++ tests/test_test_tree_browser.py
@@ -1,10 +1,13 @@
-import  adatest._test_tree_browser as ttb
+import pandas as pd
+
+import adatest._test_tree_browser as ttb
+
 
 class TestIsSubTopic:
     def test_topic_is_subtopic(self):
-        assert ttb.is_subtopic('/A', '/A/B')
-        assert ttb.is_subtopic('/A', '/A/B/C')
-        assert ttb.is_subtopic('/A', '/A/B/C/')
+        assert ttb.is_subtopic("/A", "/A/B")
+        assert ttb.is_subtopic("/A", "/A/B/C")
+        assert ttb.is_subtopic("/A", "/A/B/C/")
 
     def test_topic_is_not_subtopic(self):
-        assert not ttb.is_subtopic('/A/B', '/A/C')
+        assert not ttb.is_subtopic("/A/B", "/A/C")
