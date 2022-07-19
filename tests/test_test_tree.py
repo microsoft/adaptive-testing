494837e4e56653c9fba2fa182c7914dfabf142ab
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-07-19 10:39:13 -0400

    Assert some more

--- tests/test_test_tree.py
+++ tests/test_test_tree.py
@@ -13,3 +13,8 @@ def test_simple_init_with_list():
         {"topic": "", "type": "{} should output {}", "input": "This is good", "output": "NEGATIVE", "label": "fail"}
     ])
     assert len(tree) == 2
+    assert tree['topic'][1] == ''
+    assert tree['type'][1] == "{} should output {}"
+    assert tree['input'][1] == 'This is good'
+    assert tree['output'][1] == "NEGATIVE"
+    assert tree['label'][1] == 'fail'
