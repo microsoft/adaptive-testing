2239d29de28d1975bc3baf90c071a6eb5c4e7b54
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-07-19 10:32:50 -0400

    Add some assertions

--- tests/test_test_tree.py
+++ tests/test_test_tree.py
@@ -2,11 +2,14 @@ import adatest
 
 def test_simple_init():
     tree = adatest.TestTree()
+    assert len(tree) == 0
 
 def test_simple_init_with_file():
     tree = adatest.TestTree("temp_test_tree.csv")
+    assert len(tree) == 0
 
 def test_simple_init_with_list():
     tree = adatest.TestTree([
         {"topic": "", "type": "{} should output {}", "input": "This is good", "output": "NEGATIVE", "label": "fail"}
     ])
+    assert len(tree) == 2
