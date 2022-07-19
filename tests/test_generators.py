e69dd1eb578f8d21f868d10b6aeb336752e62fb6
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-07-19 11:47:48 -0400

    blacken

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -4,13 +4,16 @@ from transformers import pipeline
 
 from adatest import generators
 
+
 class TestTransformers:
     def test_smoke(self):
-        hf_model = pipeline('text-generation', model="EleutherAI/gpt-neo-125M")
+        hf_model = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
         target = generators.Transformers(hf_model.model, hf_model.tokenizer)
 
-        prompts = [('id A', 'Great hotel', 'positive'), ('id B', 'Bathroom too small', 'negative')]
+        prompts = [
+            ("id A", "Great hotel", "positive"),
+            ("id B", "Bathroom too small", "negative"),
+        ]
 
-
-        results = target(prompts, 'Rooms')
+        results = target(prompts, "Rooms")
         assert results is not None
