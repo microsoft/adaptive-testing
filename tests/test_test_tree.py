c60738198fb20a56e905801799ee0460cb078cde
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-07-19 10:54:32 -0400

    Test fiddling

--- tests/test_test_tree.py
+++ tests/test_test_tree.py
@@ -1,3 +1,6 @@
+import os
+import tempfile
+
 import adatest
 
 def test_simple_init():
@@ -13,8 +16,22 @@ def test_simple_init_with_list():
         {"topic": "", "type": "{} should output {}", "input": "This is good", "output": "NEGATIVE", "label": "fail"}
     ])
     assert len(tree) == 2
+    assert tree['topic'][0] == ''
     assert tree['topic'][1] == ''
+    assert tree['type'][0] is None
     assert tree['type'][1] == "{} should output {}"
+    assert tree['input'][0] == ''
     assert tree['input'][1] == 'This is good'
+    assert tree['output'][0] == ""
     assert tree['output'][1] == "NEGATIVE"
+    assert tree['label'][0] == 'topic_marker'
     assert tree['label'][1] == 'fail'
+
+def test_to_csv():
+    tree = adatest.TestTree([
+        {"topic": "", "type": "{} should output {}", "input": "This is good", "output": "NEGATIVE", "label": "fail"}
+    ])
+    with tempfile.TemporaryDirectory() as td:
+        target_file = os.path.join(td, 'adatest_out.csv')
+        tree.to_csv(target_file)
+        assert os.path.exists(target_file)
