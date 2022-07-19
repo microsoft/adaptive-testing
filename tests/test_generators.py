da71c4452eafbe9730c249ecc9fdad1b91a14ed1
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-07-19 11:45:27 -0400

    Smoke test of a generator

---
+++ tests/test_generators.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from transformers import pipeline
+
+from adatest import generators
+
+class TestTransformers:
+    def test_smoke(self):
+        hf_model = pipeline('text-generation', model="EleutherAI/gpt-neo-125M")
+        target = generators.Transformers(hf_model.model, hf_model.tokenizer)
+
+        prompts = [('id A', 'Great hotel', 'positive'), ('id B', 'Bathroom too small', 'negative')]
+
+
+        results = target(prompts, 'Rooms')
+        assert results is not None
