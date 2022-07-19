84aa34fb39ced3968d1e00b5c7fa6982ecde33ac
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-07-19 10:54:54 -0400

    blacken

--- tests/test_test_tree.py
+++ tests/test_test_tree.py
@@ -3,35 +3,55 @@ import tempfile
 
 import adatest
 
+
 def test_simple_init():
     tree = adatest.TestTree()
     assert len(tree) == 0
+
 
 def test_simple_init_with_file():
     tree = adatest.TestTree("temp_test_tree.csv")
     assert len(tree) == 0
 
+
 def test_simple_init_with_list():
-    tree = adatest.TestTree([
-        {"topic": "", "type": "{} should output {}", "input": "This is good", "output": "NEGATIVE", "label": "fail"}
-    ])
+    tree = adatest.TestTree(
+        [
+            {
+                "topic": "",
+                "type": "{} should output {}",
+                "input": "This is good",
+                "output": "NEGATIVE",
+                "label": "fail",
+            }
+        ]
+    )
     assert len(tree) == 2
-    assert tree['topic'][0] == ''
-    assert tree['topic'][1] == ''
-    assert tree['type'][0] is None
-    assert tree['type'][1] == "{} should output {}"
-    assert tree['input'][0] == ''
-    assert tree['input'][1] == 'This is good'
-    assert tree['output'][0] == ""
-    assert tree['output'][1] == "NEGATIVE"
-    assert tree['label'][0] == 'topic_marker'
-    assert tree['label'][1] == 'fail'
+    assert tree["topic"][0] == ""
+    assert tree["topic"][1] == ""
+    assert tree["type"][0] is None
+    assert tree["type"][1] == "{} should output {}"
+    assert tree["input"][0] == ""
+    assert tree["input"][1] == "This is good"
+    assert tree["output"][0] == ""
+    assert tree["output"][1] == "NEGATIVE"
+    assert tree["label"][0] == "topic_marker"
+    assert tree["label"][1] == "fail"
+
 
 def test_to_csv():
-    tree = adatest.TestTree([
-        {"topic": "", "type": "{} should output {}", "input": "This is good", "output": "NEGATIVE", "label": "fail"}
-    ])
+    tree = adatest.TestTree(
+        [
+            {
+                "topic": "",
+                "type": "{} should output {}",
+                "input": "This is good",
+                "output": "NEGATIVE",
+                "label": "fail",
+            }
+        ]
+    )
     with tempfile.TemporaryDirectory() as td:
-        target_file = os.path.join(td, 'adatest_out.csv')
+        target_file = os.path.join(td, "adatest_out.csv")
         tree.to_csv(target_file)
         assert os.path.exists(target_file)
