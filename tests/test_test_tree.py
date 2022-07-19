ad0334553b93ecd550fb9138437c5187c9baa4ed
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-07-19 14:32:32 -0400

    Try to make test more robust

--- tests/test_test_tree.py
+++ tests/test_test_tree.py
@@ -29,14 +29,23 @@ def test_simple_init_with_list():
     assert len(tree) == 2
     assert tree["topic"][0] == ""
     assert tree["topic"][1] == ""
-    assert tree["type"][0] is None
-    assert tree["type"][1] == "{} should output {}"
-    assert tree["input"][0] == ""
-    assert tree["input"][1] == "This is good"
-    assert tree["output"][0] == ""
-    assert tree["output"][1] == "NEGATIVE"
-    assert tree["label"][0] == "topic_marker"
-    assert tree["label"][1] == "fail"
+    if tree['type'][0] is None:
+        assert tree["type"][1] == "{} should output {}"
+        assert tree["input"][0] == ""
+        assert tree["input"][1] == "This is good"
+        assert tree["output"][0] == ""
+        assert tree["output"][1] == "NEGATIVE"
+        assert tree["label"][0] == "topic_marker"
+        assert tree["label"][1] == "fail"
+    else:
+        assert tree["type"][0] == "{} should output {}"
+        assert tree["type"][1] is None
+        assert tree["input"][1] == ""
+        assert tree["input"][0] == "This is good"
+        assert tree["output"][1] == ""
+        assert tree["output"][0] == "NEGATIVE"
+        assert tree["label"][1] == "topic_marker"
+        assert tree["label"][0] == "fail"
 
 
 def test_to_csv():
