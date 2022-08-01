500ea96c94b996ed2976f5e63d030712d84ab7af
Richard Edgar <riedgar@microsoft.com>
2022-08-01 17:32:07 -0400

    Parameterise the transformer model_name
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 
 from transformers import pipeline
 
@@ -6,8 +7,9 @@ from adatest import generators
 
 
 class TestTransformers:
-    def test_smoke(self):
-        hf_model = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
+    @pytest.mark.parametrize("model_name", ["EleutherAI/gpt-neo-125M"])
+    def test_smoke(self, model_name):
+        hf_model = pipeline("text-generation", model=model_name)
         target = generators.Transformers(hf_model.model, hf_model.tokenizer)
 
         prompts = [
@@ -17,7 +19,13 @@ class TestTransformers:
 
         desired_result_count = 2
 
-        results = target(prompts, "", mode="tests", topic_description="", num_samples=desired_result_count)
+        results = target(
+            prompts,
+            "",
+            mode="tests",
+            topic_description="",
+            num_samples=desired_result_count,
+        )
         assert results is not None
         assert len(results) == desired_result_count
         for item in results:
@@ -41,7 +49,14 @@ class TestOpenAI:
 
         desired_result_count = 2
 
-        results = target(prompts, "", topic_description="", mode="tests", num_samples=desired_result_count, scorer=None)
+        results = target(
+            prompts,
+            "",
+            topic_description="",
+            mode="tests",
+            num_samples=desired_result_count,
+            scorer=None,
+        )
         assert results is not None
         assert len(results) == desired_result_count
         assert "Ret 1" in results
