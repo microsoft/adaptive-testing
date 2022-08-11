cc36e589faf850a86d587aa2113401ee6e7d6002
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-08-11 07:51:21 -0400

    Skeleton script

---
+++ scripts/build_wheel.py
@@ -0,0 +1,16 @@
+import os
+import logging
+import subprocess
+
+_logger = logging.getLogger(__file__)
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    # Build the client
+
+    # Build the wheel
+
+
+if __name__ == "__main__":
+    main()
