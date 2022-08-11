9f1fa2aaa48dda6e09c324a0fb540205617bcf78
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-08-11 08:05:16 -0400

    Trying to run the scripts

--- scripts/build_wheel.py
+++ scripts/build_wheel.py
@@ -5,9 +5,27 @@ import subprocess
 _logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
 
+def build_client():
+    # Find our initial directory
+    _logger.info("Starting build_client")
+    _logger.info("Running npm install")
+    subprocess.check_call(
+        ['npm', 'install'],
+        cwd='client',
+        shell=True
+    )
+    _logger.info("Running npx webpack")
+    subprocess.check_call(
+        ['npx', 'webpack'],
+        cwd='client',
+        shell=True
+    )
+    _logger.info("Ending build_client")
 
 def main():
+    assert 'setup.py' in os.listdir(), "Must be run from repo root"
     # Build the client
+    build_client()
 
     # Build the wheel
 
