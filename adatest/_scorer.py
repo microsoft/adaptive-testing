2d39d3400daf71b2e78206d76858e0bd3ff9d73b
Scott Lundberg <scott.lundberg@microsoft.com>
2022-08-17 09:31:51 -0700

    fix template issue

--- adatest/_scorer.py
+++ adatest/_scorer.py
@@ -138,7 +138,7 @@ class ClassifierScorer(Scorer):
             out_strings[eval_inds[i]].append(self.model.output_names[np.argmax(model_out[i])])
             out_probs[eval_inds[i]].append(model_out[i])
             i += 1
-        for i in eval_inds:
+        for i in set(eval_inds):
             out_strings[i] = "|".join(out_strings[i]) # template outputs are joined by |
             out_probs[i] = np.column_stack(out_probs[i]) # the probability of a set of items is the prob of the min item
 
@@ -235,9 +235,9 @@ class GeneratorScorer(Scorer):
         out_strings = [[] for _ in range(len(eval_ids))]
         i = 0
         while i < len(model_out):
-            out_strings[eval_inds[i]].append(model_out[i])
+            out_strings[eval_inds[i]].append(str(model_out[i]))
             i += 1
-        for i in eval_inds:
+        for i in set(eval_inds):
             out_strings[i] = "|".join(out_strings[i]) # template outputs are joined by |
 
         scores = []
