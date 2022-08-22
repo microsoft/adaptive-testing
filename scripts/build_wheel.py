e58156a834dc63e09fb0b4057c2e8384f878c969
Richard Edgar <riedgar@microsoft.com>
2022-08-22 15:02:01 -0400

    Add a script to build the wheel (#28)
    * Skeleton script
    
    * Trying to run the scripts
    
    * Add in wheel building
    
    * Add cryptography to setup.py and blacken
    
    * Fix sort order
    
    * Change how processes are spawned
    
    * Don't spawn a shell?
    
    * Unused import
    
    * Add powershell note
    
    * Make script cross platform (hopefully)

---
+++ scripts/build_wheel.py
@@ -0,0 +1,40 @@
+import os
+import logging
+import platform
+import subprocess
+
+_logger = logging.getLogger(__file__)
+logging.basicConfig(level=logging.INFO)
+
+
+def build_client():
+    # Find our initial directory
+    _logger.info("Starting build_client")
+    _logger.info("Running npm install")
+    # Spawning npm appears to work differently in PowerShell
+    spawn_shell = platform.system() == "Windows"
+    subprocess.run(["npm", "install"], shell=spawn_shell, cwd="client", check=True)
+    _logger.info("Running npx webpack")
+    subprocess.run(["npx", "webpack"], shell=spawn_shell, cwd="client", check=True)
+    _logger.info("Ending build_client")
+
+
+def build_wheel():
+    _logger.info("Starting build_wheel")
+    subprocess.run(["python", "setup.py", "sdist", "bdist_wheel"], check=True)
+    _logger.info("Ending build_wheel")
+
+
+def main():
+    assert "setup.py" in os.listdir(), "Must be run from repo root"
+    # Build the client
+    build_client()
+
+    # Build the wheel
+    build_wheel()
+
+    _logger.info("Completed")
+
+
+if __name__ == "__main__":
+    main()
