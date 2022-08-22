ffb8588ef3a98624a0d2ae0585cb6234f87da907
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-08-22 13:11:58 -0400

    Change how processes are spawned

--- scripts/build_wheel.py
+++ scripts/build_wheel.py
@@ -2,40 +2,38 @@ import os
 import logging
 import subprocess
 
+from typing import List
+
 _logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
+
 
 def build_client():
     # Find our initial directory
     _logger.info("Starting build_client")
     _logger.info("Running npm install")
-    subprocess.check_call(
-        ['npm', 'install'],
-        cwd='client',
-        shell=True
-    )
+    subprocess.run(["npm", "install"], cwd="client", shell=True, check=True)
     _logger.info("Running npx webpack")
-    subprocess.check_call(
-        ['npx', 'webpack'],
-        cwd='client',
-        shell=True
-    )
+    subprocess.run(["npx", "webpack"], cwd="client", shell=True, check=True)
     _logger.info("Ending build_client")
+
 
 def build_wheel():
     _logger.info("Starting build_wheel")
-    subprocess.check_call(["python", "setup.py", "sdist", "bdist_wheel"])
+    subprocess.run(["python", "setup.py", "sdist", "bdist_wheel"], check=True)
     _logger.info("Ending build_wheel")
 
+
 def main():
-    assert 'setup.py' in os.listdir(), "Must be run from repo root"
+    assert "setup.py" in os.listdir(), "Must be run from repo root"
     # Build the client
     build_client()
 
     # Build the wheel
     build_wheel()
-    
+
     _logger.info("Completed")
+
 
 if __name__ == "__main__":
     main()
