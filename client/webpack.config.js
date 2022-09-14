78c208a5058fed2773448edbc2eb54f81a62e231
Nicholas King <nicholasking@microsoft.com>
2022-09-14 17:41:27 -0500

    Update build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
- 
+
+const isDevelopment = process.env.NODE_ENV === 'dev';
+
 module.exports = {
   entry: path.resolve(__dirname, './src/adatest.jsx'),
-  devtool: 'eval-source-map',
+  devtool: isDevelopment ? 'eval-source-map' : false,
   module: {
     rules: [
       {
@@ -11,8 +13,21 @@ module.exports = {
         use: ['babel-loader'],
       },
       {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+              loader: "ts-loader"
+          }
+        ]
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        use: ['url-loader'],
       },
       { // this allows font-awesome to be used during development mode... (since we print to the page in a script tag)
         test: /\.js$/,
@@ -31,7 +46,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
   },
   externals: {
     // 'react': 'React',
@@ -41,6 +56,5 @@ module.exports = {
     path: path.resolve(__dirname, '../adatest/resources'),
     filename: 'main.js',
   },
-  mode: "production"
-  // mode: "development"
+  mode: isDevelopment ? "development" : "production"
 };
