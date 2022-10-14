0cebb17a95fecc8384692a4d2020270de3114867
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-10-14 10:28:47 -0400

    Complete the smoke test

--- tests/test_prompt_builder.py
+++ tests/test_prompt_builder.py
@@ -1,3 +1,5 @@
+import uuid
+
 import adatest
 
 def test_test_suggestions_smoke():
@@ -7,6 +9,7 @@ def test_test_suggestions_smoke():
     target = adatest._prompt_builder.PromptBuilder(prompt_size=prompt_size)
 
     n_reps = 8
+    my_topic = '/Price'
     suggestions = target(test_tree, topic='/Price', score_column='model score', repetitions=n_reps)
     assert suggestions is not None
     assert isinstance(suggestions, list)
@@ -14,3 +17,15 @@ def test_test_suggestions_smoke():
     for s in suggestions:
         assert isinstance(s, list)
         assert len(s) == prompt_size
+        for p in s:
+            assert isinstance(p, tuple)
+            assert isinstance(p[0], str)
+            # Check first item is a uuid
+            _ = uuid.UUID(p[0])
+            # Second is the topic
+            assert isinstance(p[1], str)
+            assert p[1] == my_topic
+            # Finally the actual propsal
+            assert isinstance(p[2], str)
+            
+            
