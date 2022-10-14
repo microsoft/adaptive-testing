49bdd413dc8a98710fd7453da526a3ac0e9028bf
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-10-14 10:42:46 -0400

    Figure out what things should really be

--- tests/test_prompt_builder.py
+++ tests/test_prompt_builder.py
@@ -2,15 +2,18 @@ import uuid
 
 import adatest
 
+
 def test_test_suggestions_smoke():
     test_tree = adatest.TestTree("imdb_hotel_conversion.csv")
 
-    prompt_size=5
+    prompt_size = 5
     target = adatest._prompt_builder.PromptBuilder(prompt_size=prompt_size)
 
     n_reps = 8
-    my_topic = '/Price'
-    suggestions = target(test_tree, topic='/Price', score_column='model score', repetitions=n_reps)
+    my_topic = "/Price"
+    suggestions = target(
+        test_tree, topic=my_topic, score_column="model score", repetitions=n_reps
+    )
     assert suggestions is not None
     assert isinstance(suggestions, list)
     assert len(suggestions) == n_reps
@@ -19,13 +22,7 @@ def test_test_suggestions_smoke():
         assert len(s) == prompt_size
         for p in s:
             assert isinstance(p, tuple)
-            assert isinstance(p[0], str)
-            # Check first item is a uuid
-            _ = uuid.UUID(p[0])
-            # Second is the topic
-            assert isinstance(p[1], str)
-            assert p[1] == my_topic
-            # Finally the actual propsal
-            assert isinstance(p[2], str)
-            
-            
+            # Each tuple should reference a row in the test_true
+            tt_row = test_tree.loc[p[0]]
+            assert tt_row["topic"] == p[1]
+            assert tt_row["input"] == p[2]
