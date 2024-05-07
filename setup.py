75a89e904acd4754a56d8f9978a747e8bd0182c5
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-05-07 18:13:12 -0400

    Tweak setup.py

--- setup.py
+++ setup.py
@@ -21,12 +21,15 @@ def find_version(*file_paths):
 
 setup(
     name="adatest",
-    version=find_version("adatest", "__init__.py"),
-    url="https://github.com/microsoft/adatest.git",
+    version=find_version("adaptivetesting", "__init__.py"),
+    url="https://github.com/microsoft/adaptive-testing.git",
     author="Scott Lundberg and Marco Tulio Ribeiro",
     author_email="scott.lundberg@microsoft.com",
     description="Adaptively test and debug any natural language machine learning model.",
-    packages=find_packages(exclude=["user_studies", "notebooks", "client"]),
+    packages=find_packages(
+        exclude=["user_studies", "notebooks", "client"],
+        entry_points={"console_scripts": ["adatest = adaptivetesting.__main__:main"]},
+    ),
     package_data={"adatest": ["resources/*"]},
     install_requires=[
         "aiohttp",
@@ -46,9 +49,9 @@ setup(
         "dev": [
             "black",
             "flake8",
-            "openai",
+            "openai<1",
             "datasets",
-            "transformers",
+            "transformers<4.26",
             "pytest",
             "pytest-mock",
             "torch",
